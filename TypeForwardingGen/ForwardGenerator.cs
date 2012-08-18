7d6a8f275575eb23af907f380ff652d20c9d7172
Lukas <karim.luccin@gmail.com>
2012-08-18 12:02:45 +0200

    Fixed sln and namespace

--- TypeForwardingGen/ForwardGenerator.cs
+++ TypeForwardingGen/ForwardGenerator.cs
@@ -66,7 +66,7 @@ namespace TypeForwardingGen
         public void Generate(StreamWriter dest_writer, string[] assemblyNames)
         {
             dest_writer.WriteLine("using System;");
-            dest_writer.WriteLine("using System.Runtime.CompilerService");
+            dest_writer.WriteLine("using System.Runtime.CompilerServices");
             dest_writer.WriteLine();
             dest_writer.WriteLine();
 
