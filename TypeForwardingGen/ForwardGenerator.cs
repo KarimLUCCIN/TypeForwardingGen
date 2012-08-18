1c33763e85b314cd3f982b4d9f781e189f5be5e7
Lukas <karim.luccin@gmail.com>
2012-08-18 11:59:55 +0200

    Initial commit

---
+++ TypeForwardingGen/ForwardGenerator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TypeForwardingGen
+{
+    public class ForwardGenerator
+    {
+        private IEnumerable<Type> GetAllPublicTypes(string[] assemblyNames)
+        {
+            foreach (var assemblyName in assemblyNames)
+            {
+                Console.WriteLine("Processing {0}", assemblyName);
+
+                Assembly assembly;
+
+                if (File.Exists(assemblyName))
+                    assembly = Assembly.LoadFile(assemblyName);
+                else
+                    assembly = Assembly.LoadWithPartialName(assemblyName);
+
+                if (assembly == null)
+                    throw new InvalidOperationException(String.Format("Could not load {0}", assemblyName));
+
+                foreach (var assembly_type in assembly.GetTypes())
+                {
+                    if (assembly_type.IsPublic)
+                    {
+                        yield return assembly_type;
+                    }
+                }
+            }
+
+            Console.WriteLine("Finished");
+        }
+
+        /// <summary>
+        /// Generate a C# file source containing a [assembly:TypeForwardedToAttribute(typeof(Example))] attribute for each
+        /// types of the specified dlls
+        /// </summary>
+        /// <param name="dest_file"></param>
+        /// <param name="assemblyNames"></param>
+        public void Generate(string dest_file, string[] assemblyNames)
+        {
+            using (var dest_stream = new FileStream(dest_file, FileMode.Create))
+            {
+                using (var dest_writer = new StreamWriter(dest_stream))
+                {
+                    Generate(dest_writer, assemblyNames);
+
+                    dest_writer.Flush();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Generate a C# file source containing a [assembly:TypeForwardedToAttribute(typeof(Example))] attribute for each
+        /// types of the specified dlls
+        /// </summary>
+        /// <param name="dest_writer"></param>
+        /// <param name="assemblyNames"></param>
+        public void Generate(StreamWriter dest_writer, string[] assemblyNames)
+        {
+            dest_writer.WriteLine("using System;");
+            dest_writer.WriteLine("using System.Runtime.CompilerService");
+            dest_writer.WriteLine();
+            dest_writer.WriteLine();
+
+            dest_writer.WriteLine("/* Type Forwarded from");
+
+            foreach (var assemblyName in assemblyNames)
+            {
+                dest_writer.WriteLine(String.Format("\t{0}", assemblyName));
+            }
+
+            dest_writer.WriteLine(" */");
+
+            dest_writer.WriteLine();
+            dest_writer.WriteLine();
+
+            foreach (var asm_type in GetAllPublicTypes(assemblyNames))
+            {
+                dest_writer.WriteLine(String.Format("[assembly:TypeForwardedToAttribute(typeof({0}))]", asm_type.FullName));
+            }
+        }
+    }
+}
---
+++ TypeForwardingGen/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TypeForwardingGen
+{
+    class Program
+    {
+        static int Usage()
+        {
+            Console.WriteLine("Usage :");
+            Console.WriteLine("TypeForwardingGen dst_file.cs assembly1 assembly2 ...");
+            Console.WriteLine("dst_file.cs : name of the generated C# file");
+            Console.WriteLine("assembly 1 .. n : assemblies which will have their types forwarding from the generated C# file");
+            return 1;
+        }
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine("TypeForwarding Assembly generator");
+            Console.WriteLine("Copyright (c) Karim Audrey Luccin");
+
+            if (args == null || args.Length < 2)
+            {
+                Console.WriteLine("Invalid argument counts");
+                return Usage();
+            }
+            else
+            {
+                try
+                {
+                    var dest_file = args[0];
+
+                    var assemblyNames = new string[args.Length - 1];
+
+                    for (int i = 1; i < args.Length; i++)
+                        assemblyNames[i-1] = args[i];
+
+                    var generator = new ForwardGenerator();
+                    generator.Generate(dest_file, assemblyNames);
+
+                    return 0;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    return 2;
+                }
+            }
+        }
+    }
+}
